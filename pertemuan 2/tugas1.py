4cb7e4fe88a702617f3a61cc5606a28cfdb13f36
lukman-hehe <146632558+lukman-hehe@users.noreply.github.com>
2024-09-27 11:04:44 +0700

    Add files via upload

---
+++ pertemuan 2/tugas1.py
@@ -0,0 +1,6 @@
+total_belanja = float(input("Masukkan total belanja Anda: "))
+
+if total_belanja > 100000:
+    print("Selamat, Anda dapat hadiah!")
+else:
+    print("Terima kasih sudah berbelanja.")
---
+++ pertemuan 2/tugas2..py
@@ -0,0 +1,9 @@
+password_benar = "12345"  # Password yang benar
+input_password = input("Masukkan password: ")
+
+if input_password == password_benar:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+    
+print("Terima kasih sudah menggunakan aplikasi kami.")
